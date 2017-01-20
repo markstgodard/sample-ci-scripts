21ea2137292da3e035e9738f71e549b45eb2e41c
Mark St. Godard <markst.godard@boldcommerce.com>
2017-01-19 21:00:33 -0600

    test

--- create-rc.sh
+++ create-rc.sh
@@ -4,6 +4,9 @@ set -e
 
 cd backend-dev
 
+git config --global user.email "markstgodard@gmail.com"
+git config --global user.name "Mark St.Godard"
+
 echo "test" >> test.txt
 git add test.txt
 git commit -m"test"
